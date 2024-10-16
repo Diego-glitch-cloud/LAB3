bf32392b34df1b23853c95bafbbd3064e898a41f
Diego-glitch-cloud <dcalderon918@gmail.com>
2024-10-15 23:19:42 -0600

    creación del menú y programación defensiva

--- Main.java
+++ Main.java
@@ -4,9 +4,27 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion; // opción elegida en el menú
+        boolean continuar = true; 
+
+
+        String menu = """
+                Seleccione el número correspondiente a la opción que desea realizar: 
+
+                1 = Listar vehículos por categoría
+                2 = Buscar vehículo por ID 
+                3 = Agregar vehículo
+                4 = Eliminar Vehículo
+                5 = Mostrar estado de los vehículos
+                6 = Calcular Monto total en Quetzales por vehículos según su estado
+                7 = Salir
+                """;
 
         String autoscsv = "autos.csv";
         String motoscsv = "motocicletas.csv";
@@ -21,10 +39,41 @@ public class Main {
         motos = leerCsv(motoscsv);
         camiones = leerCsv(camionescsv);
 
+        while (continuar) {
+            System.out.println(menu);
+            System.out.print("Ingrese la opción que desea ejecutar: ");
+            
+            // Comprobar si la entrada es un número
+            if (scanner.hasNextInt()) {
+                opcion = scanner.nextInt();
+                // Verificar si el número está dentro del rango permitido por el menú
+                if (opcion >= 1 && opcion <= 7) {
+
+                    // switch switch switch 
+                    switch (opcion) {
+                        case 1: 
+                        case 2:
+                        case 3:
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                            continuar = false;
+                            System.out.println("Saliendo...");
+                    }
+                    
+                } else {
+                    System.out.println("Opción no válida. Vuelva a intentarlo.");
+                }
+            } else {
+                // Si la entrada no es un número
+                System.out.println("Opción no válida. Vuelva a intentarlo.");
+                scanner.next(); // Limpiar el escáner
+            }
+        }
+
         
-
-
-
+        scanner.close(); // Cerrar el escáner
 
         
     }
